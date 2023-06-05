009aa291a6869c15d70525736427b29b3284e6e2
julfinch <julfinch@gmail.com>
2023-06-05 21:32:54 +0800

    update Homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { 
@@ -7,6 +7,7 @@ import {
     SearchIcon,
     AdjustmentsIcon,
 } from 'react-native-heroicons/outline' 
+
 
 const HomeScreen = () => {
     const navigation = useNavigation();
@@ -19,9 +20,8 @@ const HomeScreen = () => {
 
     return (
         <SafeAreaView className="bg-white pt-5">
-            <Text className="text-red-500">
                 {/* Header */}
-                <View className="flex-row pb-3 items-center mx-4 space-x-2">
+                <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
                     <Image
                         source={{
                             uri: "https://links.papareact.com/wru",
@@ -41,7 +41,29 @@ const HomeScreen = () => {
 
                     <UserIcon size={35} color="#00CCBB"/>
                 </View>
-            </Text>
+
+                {/* SEARCH */}
+                <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+                    <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+                        <SearchIcon color="gray" size={20}/>
+                        <TextInput 
+                            placeholder='Restaurants and cuisines'
+                            keyboardType='default'
+                        />
+                    </View>
+
+                    <AdjustmentsIcon color="#00CCBB"/>
+                </View>
+
+                {/* BODY */}
+                <ScrollView
+                    className="bg-gray-100"
+                    contentContainerStyle={{
+                    paddingBottom: 100,
+                    }}
+                >
+                    {/* Catagary */}
+                </ScrollView>
         </SafeAreaView>
     )
 }
